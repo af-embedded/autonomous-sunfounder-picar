7814325e7680228f093c7411bfc42ce947910318
Abdullah Baig <abdullahbaig456@gmail.com>
2019-03-21 16:18:18 +0100

    Fix comment block indentation

--- utils/IMUCalibration.py
+++ utils/IMUCalibration.py
@@ -4,30 +4,30 @@ from sensors.IMU import IMU
 
 class IMUCalibration:
     def __init__(self):
-    """
-        IMU Calibration class
-        Originally written for MPU-9250 9-DOF IMU Sensor
-        
-        This class calibrates the IMU by taking a sequence of
-        readings and calculating the mean of the readings.
-        The mean readings are then passed to the IMU for calibration.
-        
-        Before calibrating the IMU, make sure that the IMU is fitted
-        properly and is at rest. If the orientation of the IMU is changed
-        then it will need to be recalibrated
-    """
+        """
+            IMU Calibration class
+            Originally written for MPU-9250 9-DOF IMU Sensor
+            
+            This class calibrates the IMU by taking a sequence of
+            readings and calculating the mean of the readings.
+            The mean readings are then passed to the IMU for calibration.
+            
+            Before calibrating the IMU, make sure that the IMU is fitted
+            properly and is at rest. If the orientation of the IMU is changed
+            then it will need to be recalibrated
+        """
         self._readings = []
-    
-    def calibrate(self, imu, iterations, sleep_seconds=0):
-    """
-        Calibrate the given IMU.
-        The IMU must be fitted properly and should be stationary
-        in its rest position
         
-        :param imu: The IMU object
-        :param iterations: The number of readings to take
-        :param sleep_seconds: Delay between each reading
-    """
+    def calibrate(self, imu, iterations, sleep_seconds=0):
+        """
+            Calibrate the given IMU.
+            The IMU must be fitted properly and should be stationary
+            in its rest position
+            
+            :param imu: The IMU object
+            :param iterations: The number of readings to take
+            :param sleep_seconds: Delay between each reading
+        """
         for i in range(iterations):
             imu.tick()
             self._readings.append(imu.get_reading())
