d73ea8ca29547426edc94ac92f791d182aa77658
horaceklai <44500643+horaceklai@users.noreply.github.com>
2019-04-12 11:19:06 +0200

    updated

--- camera/LaneTrackingImage.py
+++ camera/LaneTrackingImage.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 from LaneTracking import process_one_frame
 import matplotlib.pyplot as plt
-
+import math
 
 # load image
 this_file_path = os.path.dirname(os.path.abspath(__file__))
@@ -21,49 +21,47 @@ for filename in image_files:
     angle = 0
     angle_max = 45
     x_int_mid_min = 50
-    m_max_actuate = 2
     x_int_max_actuate = 100
     try:
         print(averaged_lines)
         x1, y1, x2, y2 = averaged_line
         m = (y2 - y1) / (x2 - x1)
-        b = y1 - m*x1
-        x_int_mid = (image.shape[1]-b)/m - image.shape[1]/2
+        m_angle = math.degrees(math.atan(1 / m))  # left is positive
+        b = y1 - m * x1
+        x_int_mid = (image.shape[1] - b) / m - image.shape[1] / 2
         print('slope', m)
-        print('x-intercept', (image.shape[1]-b)/m)
+        print('x-intercept', (image.shape[1] - b) / m)
         print('x-int from middle', x_int_mid)
 
-
-        if m > 0:
-            # go straight if slope is high and x-int is on the right
-            if abs(m) > m_max_actuate and abs(x_int_mid) > 0:
+        if m_angle > 0:
+            # go straight if x-int is too far right
+            if x_int_mid > x_int_max_actuate:
                 print('straight')
                 angle = 0
-            # turn left if slope is too low or x-int is too left of the image
+            # turn left
             else:
                 print('turn')
-                angle_slope = 0.4 * (m_max_actuate - abs(m)) * angle_max
-                angle_x_int = 1 * (x_int_mid_min / max(0.01, x_int_mid)) * angle_max
-                angle = max(angle_slope, angle_x_int)
-                # print(angle_slope, angle_x_int)
-        elif m < 0:
-            # go straight if slope is high and x-int is on the right
-            if abs(m) > m_max_actuate and abs(x_int_mid) > 0:
+                angle = m_angle
+        elif m_angle < 0:
+            # go straight if x-int is on the left
+            if x_int_mid < -x_int_max_actuate:
                 print('straight')
                 angle = 0
-            # turn right if abs(slope) is too low or x-int is too right of the image
+            # turn right
             else:
                 print('turn')
-                angle_slope = 0.4 * (m_max_actuate - abs(m)) * angle_max
-                angle_x_int = 1 * (-x_int_mid_min / min(-0.01, x_int_mid)) * angle_max
-                angle = max(angle_slope, angle_x_int)
+                angle = -m_angle
+        else:
+            print('straight')
+            angle = 0
 
     except:
         print('no averaged line')
 
-    print(angle)
+    # Actuate, angles in the car go from 45 (full left) to 135 (full right)
     angle = angle + 90
     angle = max(min(angle, 135), 45)
+    print(angle)
 
     # show image
     cv2.imshow('result', output_image)
