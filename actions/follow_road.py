406dd3ca019c3119bfdea479c0fc80053db6d4d4
horaceklai <44500643+horaceklai@users.noreply.github.com>
2019-04-17 22:21:27 +0200

    added setup and requirement files

--- actions/follow_road.py
+++
@@ -1,3 +0,0 @@
-from .picar import back_wheels, front_wheels
-
-fw = front_wheels.Front_wheels(debug=False, db=db)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='autonomous-sunfounder-picar',
+    version='',
+    packages=['picar', 'picar.SunFounder_TB6612', 'picar.SunFounder_PCA9685', 'utils', 'sensors', 'sensors.MPU9250'],
+    url='',
+    license='',
+    author='Horace Lai',
+    author_email='horace.lai@afconsult.com',
+    description='setup file for new comers to the project'
+)
